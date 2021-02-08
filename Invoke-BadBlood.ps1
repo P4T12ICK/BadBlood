96537d68668d49d044c5b634378f5954c73007cb
P4T12ICK <pbareib@splunk.com>
2021-02-08 13:20:18 +0100

    changes

--- Invoke-BadBlood.ps1
+++ Invoke-BadBlood.ps1
@@ -10,16 +10,16 @@
     .NOTES
        Written by David Rowe, Blog secframe.com
        Twitter : @davidprowe
-       I take no responsibility for any issues caused by this script.  I am not responsible if this gets run in a production domain. 
+       I take no responsibility for any issues caused by this script.  I am not responsible if this gets run in a production domain.
       Thanks HuskyHacks for user/group/computer count modifications.  I moved them to parameters so that this tool can be called in a more rapid fashion.
     .FUNCTIONALITY
        Adds a ton of stuff into a domain.  Adds Users, Groups, OUs, Computers, and a vast amount of ACLs in a domain.
     .LINK
        http://www.secframe.com/badblood
-   
+
     #>
 [CmdletBinding()]
-    
+
 param
 (
    [Parameter(Mandatory = $false,
@@ -51,41 +51,13 @@ $totalscripts = 7
 
 $i = 0
 cls
-write-host "Welcome to BadBlood"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-write-host "The first tool that absolutely mucks up your TEST domain"
-write-host "This tool is never meant for production and can totally screw up your domain"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
-Write-Host  'Press any key to continue...';
-write-host "You are responsible for how you use this tool. It is intended for personal use only"
-write-host "This is not intended for commercial use"
-Write-Host  'Press any key to continue...';
-write-host "`n"
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
-write-host "`n"
-write-host "Domain size generated via parameters `n Users: $UserCount `n Groups: $GroupCount `n Computers: $ComputerCount"
-write-host "`n"
-
-$badblood = Read-Host -Prompt "Type `'badblood`' to deploy some randomness into a domain"
+$badblood = 'badblood'
 $badblood.tolower()
 if ($badblood -ne 'badblood') { exit }
 if ($badblood -eq 'badblood') {
    $Domain = Get-addomain
 
-   # LAPS STUFF
-   if ($PSBoundParameters.ContainsKey('SkipLapsInstall') -eq $false)
-      {
-         Write-Progress -Activity "Random Stuff into A domain" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
-         .($basescriptPath + '\AD_LAPS_Install\InstallLAPSSchema.ps1')
-         Write-Progress -Activity "Random Stuff into A domain: Install LAPS" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
-      }
-   else{}
-   
    $I++
 
 
@@ -101,7 +73,7 @@ if ($badblood -eq 'badblood') {
    # User Creation
    $ousAll = Get-adorganizationalunit -filter *
    write-host "Creating Users on Domain" -ForegroundColor Green
-    
+
    $x = 1
    Write-Progress -Activity "Random Stuff into A domain - Creating Users" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
    $I++
@@ -121,7 +93,7 @@ if ($badblood -eq 'badblood') {
    Write-Progress -Activity "Random Stuff into A domain - Creating $GroupCount Groups" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
    $i++
    .($basescriptPath + '\AD_Groups_Create\CreateGroups.ps1')
-    
+
    do {
       Creategroup
       Write-Progress -Activity "Random Stuff into A domain - Creating $GroupCount Groups" -Status "Progress:" -PercentComplete ($x / $GroupCount * 100)
@@ -143,22 +115,22 @@ if ($badblood -eq 'badblood') {
       $x++
    }while ($x -lt $ComputerCount)
    $Complist = get-adcomputer -filter *
-    
+
 
    #Permission Creation of ACLs
    $I++
    write-host "Creating Permissions on Domain" -ForegroundColor Green
    Write-Progress -Activity "Random Stuff into A domain - Creating Random Permissions" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
    .($basescriptPath + '\AD_Permissions_Randomizer\GenerateRandomPermissions.ps1')
-    
-    
+
+
    # Nesting of objects
    $I++
    write-host "Nesting objects into groups on Domain" -ForegroundColor Green
    .($basescriptPath + '\AD_Groups_Create\AddRandomToGroups.ps1')
    Write-Progress -Activity "Random Stuff into A domain - Adding Stuff to Stuff and Things" -Status "Progress:" -PercentComplete ($i / $totalscripts * 100)
    AddRandomToGroups -Domain $Domain -Userlist $AllUsers -GroupList $Grouplist -LocalGroupList $LocalGroupList -complist $Complist
-    
+
 
 
 }
